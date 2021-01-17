e1112c4b189bdbbaebf2fce1334773d2e020992b
shuzhili <1174458397@qq.com>
2021-01-17 17:26:13 +0800

    添加AudioRecord音频录制

---
+++ app/src/main/java/cn/live/livetest/audio/AudioProcessor.java
@@ -0,0 +1,67 @@
+package cn.live.livetest.audio;
+
+import android.media.AudioRecord;
+
+import java.util.Arrays;
+
+public class AudioProcessor extends Thread {
+
+    private volatile boolean mPauseFlag;
+    private volatile boolean mStopFlag;
+    private volatile boolean mMute;
+    private AudioRecord mAudioRecord;
+    private AudioEncoder mAudioEncoder;
+    private byte[] mRecordBuffer;
+    private int mRecordBufferSize;
+
+    public AudioProcessor(AudioRecord audioRecord, AudioConfiguration audioConfiguration) {
+        mRecordBufferSize = AudioUtils.getRecordBufferSize(audioConfiguration);
+        mRecordBuffer = new byte[mRecordBufferSize];
+        mAudioRecord = audioRecord;
+        mAudioEncoder = new AudioEncoder(audioConfiguration);
+        mAudioEncoder.prepareEncoder();
+    }
+
+    public void setAudioEncoderListener(OnAudioEncodeListener listener) {
+        mAudioEncoder.setOnAudioEncodeListener(listener);
+    }
+
+    public void stopEncode() {
+        mStopFlag = true;
+        if (mAudioEncoder != null) {
+            mAudioEncoder.stop();
+            mAudioEncoder = null;
+        }
+    }
+
+    public void pauseEncode(boolean pause) {
+        mPauseFlag = pause;
+    }
+
+    public void setMute(boolean mute) {
+        mMute = mute;
+    }
+
+    @Override
+    public void run() {
+        while (!mStopFlag) {
+            while (mPauseFlag) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            int readLen = mAudioRecord.read(mRecordBuffer, 0, mRecordBufferSize);
+            if (readLen > 0) {
+                if (mMute) {
+                    byte clearM = 0;
+                    Arrays.fill(mRecordBuffer, clearM);
+                }
+                if (mAudioEncoder != null) {
+                    mAudioEncoder.offerEncoder(mRecordBuffer);
+                }
+            }
+        }
+    }
+}
